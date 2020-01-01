a1ef26bef4fcfb03201336ffc5b927a115489f4c
seejins <sejin@Sejins-MacBook-Pro.local>
2019-12-31 21:09:40 -0800

    initial commit

---
+++ src/store.js
@@ -0,0 +1,20 @@
+const bookmarks = [
+    {
+        id: 1,
+        title: 'Google',
+        url: 'http://www.google.com',
+        description: 'Search engine',
+        rating: 5
+    },
+
+    {
+        id: 2,
+        title: 'Bing',
+        url: 'http://www.bing.com',
+        description: 'the other search engine',
+        rating: 1
+    }
+
+]
+
+module.exports = { bookmarks }
